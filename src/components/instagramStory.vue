dd99127d146c7d3befdbce680968eec0cda94950
Sven Gerenčir <svengerencir@hotmail.com>
2022-02-17 01:40:03 +0100

    VUE 03
    dinamična pretraga riješena, nije case sensitive, po autoru i po naslovu provjerava

--- src/components/instagramStory.vue
+++ src/components/instagramStory.vue
@@ -1,38 +1,33 @@
 <template>
   <div class="card text-center bg-ljubicasta mt-5">
+    <div class="text-white funfont text-center d-block pt-2">
+      <h4>{{ info.storyTitle }}</h4>
+    </div>
     <div class="card-header row m-0 p-2">
       <div class="ml-1 mr-auto d-inline-block">
         <img
           class="d-inline"
-          src="@/assets/profiltemplate.png"
+          :src="info.storyUser.profile"
           width="32px"
           height="32px"
           alt="profile picture"
         />
         <p class="funfont text-white align-middle d-inline">
-          &nbsp;someonesUsername
+          &nbsp;{{ info.storyUser.name }}
         </p>
       </div>
       <button class="btn text-white btn-naranca rounded-circle">...</button>
     </div>
-    <div class="card-body">
-      <img class="card-img-top" src="@/assets/kavica.jpg" alt="cup of coffee" />
+    <div class="card-body m-0 p-0">
+      <img class="card-img-top" :src="info.storyPic" alt="cup of coffee" />
     </div>
-    <div class="card-footer funfont text-naranca">2 days ago</div>
+    <div class="card-footer funfont text-naranca">{{ info.storyDate }}</div>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["info"],
   name: "InstagramStory",
-  components: {},
 };
 </script>
-<style scoped>
-.card-body {
-  padding: 0px !important;
-}
-.card-img-top {
-  padding: 0px !important;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,7 @@
     <div class="row pt-5">
       <div class="col-1 col-md-3"></div>
       <div class="col-7 col-md-4">
-        <instagramStory />
-        <instagramStory />
-        <instagramStory />
+        <instagramStory v-for="card in filteredCards" :info="card" />
       </div>
       <div class="col-2 col-md-3">
         <onSide />
@@ -15,13 +13,58 @@
 </template>
 
 <script>
-import jumbotronHome from "@/components/jumbotronHome.vue";
 import instagramStory from "@/components/instagramStory.vue";
 import onSide from "@/components/onSide.vue";
+import store from "@/store.js";
 export default {
   name: "Home",
+  data() {
+    return {
+      cards: [
+        {
+          storyUser: {
+            name: "Mario",
+            profile: require("@/assets/prof1.png"),
+          },
+          storyTitle: "Grize se",
+          storyDate: "an hour ago",
+          storyPic: "https://picsum.photos/id/1/400/400",
+        },
+        {
+          storyUser: {
+            name: "Mtumbu",
+            profile: require("@/assets/prof2.png"),
+          },
+          storyTitle: "Delo delo DELO",
+          storyDate: "yesterday",
+          storyPic: "https://picsum.photos/id/2/400/400",
+        },
+        {
+          storyUser: {
+            name: "Nikola",
+            profile: require("@/assets/prof3.png"),
+          },
+          storyTitle: "Još uvijek dolaze zadaće?",
+          storyDate: "two weeks ago",
+          storyPic: "https://picsum.photos/id/3/400/400",
+        },
+      ],
+    };
+  },
+  computed: {
+    filteredCards() {
+      return this.cards.filter(
+        (card) =>
+          card.storyTitle
+            .toLowerCase()
+            .includes(store.searchTerm.toLowerCase()) ||
+          card.storyUser.name
+            .toLowerCase()
+            .includes(store.searchTerm.toLowerCase())
+      );
+    },
+  },
   components: {
-    jumbotronHome,
     instagramStory,
     onSide,
   },
